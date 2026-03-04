3872f57be796d771c0f86b4976d5f2c9ef94a0df
Ella-Connolly <111038742+Ella-Connolly@users.noreply.github.com>
2026-03-04 15:51:28 -0700

    Added SocFaker

---
+++ src/generate-logs.py
@@ -0,0 +1,30 @@
+import csv
+import pprint
+from socfaker import SocFaker
+
+sc = SocFaker()
+
+header = ['Summary', 'Signature', 'Timestamp', 'Type', 'Status', 'Direction', 'Location', 'Source_IP']
+
+alert_data = []
+for _ in range(10):
+    row = [
+         sc.alert.summary, 
+         sc.alert.signature_name, 
+         sc.timestamp.current,
+         sc.alert.type,
+         sc.alert.status,
+         sc.alert.direction,
+         sc.alert.location,
+         sc.network.source.ip
+    ]
+    alert_data.append(row)
+
+#pprint(alert)
+
+with open("alert.csv", "w", encoding="UTF-8", newline="") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(header)
+    csv_writer.writerows(alert_data)
+
+print("alert.csv generated successfully.")
