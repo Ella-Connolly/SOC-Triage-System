09e5a67496d0c16cf9c747ae34694375d02a9caa
twp34 <166484691+twp34@users.noreply.github.com>
2026-03-24 15:52:52 -0700

    Update log generation to output to data/alert.csv

--- src/generate-logs.py
+++ src/generate-logs.py
@@ -1,5 +1,4 @@
 import csv
-import pprint
 from socfaker import SocFaker
 
 sc = SocFaker()
@@ -7,23 +6,22 @@ sc = SocFaker()
 header = ['Summary', 'Signature', 'Timestamp', 'Type', 'Status', 'Direction', 'Location']
 
 alert_data = []
-for _ in range(10):
+
+for _ in range(50):
     row = [
-         sc.alert.summary, 
-         sc.alert.signature_name, 
-         sc.timestamp.current,
-         sc.alert.type,
-         sc.alert.status,
-         sc.alert.direction,
-         sc.alert.location
+        sc.alert.summary, 
+        sc.alert.signature_name, 
+        sc.timestamp.current,
+        sc.alert.type,
+        sc.alert.status,
+        sc.alert.direction,
+        sc.alert.location
     ]
     alert_data.append(row)
 
-#pprint(alert)
-
-with open("alert.csv", "w", encoding="UTF-8", newline="") as csv_file:
+with open("data/alert.csv", "w", encoding="UTF-8", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(header)
     csv_writer.writerows(alert_data)
 
-print("alert.csv generated successfully.")
+print("data/alert.csv generated successfully.")
