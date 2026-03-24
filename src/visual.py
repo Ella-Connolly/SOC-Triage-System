98aef12d661b96b66a318e5accce6be7b93d4ffd
Ella-Connolly <111038742+Ella-Connolly@users.noreply.github.com>
2026-03-24 15:58:58 -0700

    Update visual.py

---
+++ src/visual.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/final_triage_report.csv')
+
+priority_counts = df['Priority'].value_counts()
+
+plt.figure(figsize=(8, 6))
+    
+plt.bar(priority_counts.index, priority_counts.values, color=['skyblue', 'red'])
+
+plt.title('SOC Triage: Incident Priority Levels')
+plt.xlabel('Priority Level')
+plt.ylabel('Number of Incidents')
+plt.savefig('data/priority_chart.png') # Saves the image for your slides
+print("Success: Chart saved to data/priority_chart.png")
+plt.show()
+
+top_5 = df.sort_values(by='Alert_Count', ascending=False).head(5)
+
+plt.figure(figsize=(10, 6))
+
+plt.bar(top_5['Location'], top_5['Alert_Count'], color='orange')
+
+plt.title('Top 5 Most Attacked Locations')
+plt.xlabel('Location Name')
+plt.ylabel('Number of Alerts')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig('data/top_5_attacks.png')
+print("Success: Top 5 chart saved to data/top_5_attacks.png")
+plt.show()
