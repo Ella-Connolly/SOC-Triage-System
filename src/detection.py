e19b269084c70e35940875c7a3189a2d3a5294eb
twp34 <166484691+twp34@users.noreply.github.com>
2026-04-07 15:41:12 -0700

    Update detection.py

--- src/detection.py
+++ src/detection.py
@@ -3,17 +3,31 @@ import pandas as pd
 def detect_threats(file_path):
     df = pd.read_csv(file_path)
 
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+
     df['Threat_Type'] = "NONE"
 
     location_counts = df['Location'].value_counts()
+    suspicious_locations = location_counts[location_counts > 5].index
 
-    suspicious_locations = location_counts[location_counts > 3].index #checks for repeated alerts from the same location 3 times within a single log file (we generate 50 logs)
+    df['Repeated_Location'] = df['Location'].isin(suspicious_locations) #checks for repeated alerts from the same location 3 times within a single log file (we generate 400 logs)
 
-    df.loc[df['Location'].isin(suspicious_locations), 'Threat_Type'] = "SAME_LOCATION_REPEATED_ALERTS"
+    df['Suspicious_Signature'] = df['Signature'].str.contains("SQL|Injection|Brute|Trojan|Ransomware|Exploit|Hack", case=False, na=False) #checks signature for common attack vectors
 
-    df.loc[df['Signature'].str.contains("SQL|Injection|Brute"), 'Threat_Type'] = "COMMON_ATTACK_VECTORS" #checks signature for common attack vectors
+    df = df.sort_values(by='Timestamp')
+
+    df['Time_Diff'] = df.groupby('Location')['Timestamp'].diff().dt.total_seconds()
+
+    df['Rapid_Activity'] = df['Time_Diff'] < 300
+
+    df.loc[(df['Repeated_Location']) & (df['Suspicious_Signature']) & (df['Rapid_Activity']), 'Threat_Type'] = "HIGH_CONFIDENCE_OF_ATTACK" #repeated locations + suspicious signature + rapid activity
+
+    df.loc[(df['Repeated_Location']) & (df['Suspicious_Signature']) & (df['Threat_Type'] == "NONE"), 'Threat_Type' ] = "MEDIUM_CONFIDENCE_OF_ATTACK" #repeated locations + suspicious signature but no rapid activity
+
+    df.loc[(df['Suspicious_Signature']) & (df['Threat_Type'] == "NONE"), 'Threat_Type'] = "LOW_CONFIDENCE_OF_ATTACK" #suspicious signature but no repeated locations or rapid activity
 
     return df
+
 
 if __name__ == "__main__":
     df = detect_threats('data/preprocessed_alerts.csv')
